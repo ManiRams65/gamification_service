32f5b26db64b8c0007ded40602876b1512b08874
Manikandan <svvmanikandan@gmail.com>
2023-11-30 02:47:23 +0530

    Update constants.js

--- src/foundation/common/constants.js
+++ src/foundation/common/constants.js
@@ -2,10 +2,10 @@ module.exports = CONSTANTS = {
   allowedOrigins: ["http://localhost:1234"],
   port: 3000,
   db_config: {
-    user: "postgres",
-    host: "localhost",
+    user: "default",
+    host: "ep-billowing-tree-69057902-pooler.us-east-1.postgres.vercel-storage.com",
     database: "gamification_schema",
-    password: "*****",
+    password: "4VNduS5IUbGH",
     port: 5432,
   },
 };
